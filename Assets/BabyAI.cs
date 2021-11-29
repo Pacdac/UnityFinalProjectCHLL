0f8efa79b992de909ea1140b82476de9476b09d3
SarahHeo <sarah.heouaine@gmail.com>
2021-11-29 11:27:15 +0100

    Baby movements (fixed + random danger)

---
+++ Assets/BabyAI.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BabyAI : MonoBehaviour
+{
+    // Suis les points de "paths", puis va chercher un danger random dans la
+    [SerializeField] private GameObject paths;
+    [SerializeField] private GameObject danger;
+    private Transform[] pathPoints;
+    private Transform[] dangerPoints;
+    private Transform movePoint;
+    private int pointIndex;
+    private float speed = 5f;
+    private int roomIndex = -1; // le mettre dans GameManager maybe
+
+    void Start()
+    {
+        getNextRoomPoints();
+    }
+
+    void Update()
+    {
+        move();
+        rotate();
+
+        bool hasReachPoint = Vector3.Distance(transform.position, movePoint.position) == 0;
+        if (hasReachPoint)
+        {
+            setNextPoint();
+        }
+    }
+
+
+    private void move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, speed * Time.deltaTime);
+    }
+
+    private void rotate()
+    {
+        Vector3 position = movePoint.position - transform.position;
+        float angle = Mathf.Atan2(position.x, position.z) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, angle - 90, 0);
+    }
+    private void setNextPoint()
+    {
+        pointIndex++;
+        bool noMorePathPoints = pointIndex >= pathPoints.Length;
+        if (noMorePathPoints)
+        {
+            lookForDanger();
+            return;
+        }
+        movePoint = pathPoints[pointIndex];
+    }
+
+    private void lookForDanger()
+    {
+        Transform roomDangerPoints = danger.transform.GetChild(roomIndex);
+        dangerPoints = roomDangerPoints.GetComponentsInChildren<Transform>();
+        int nbDanger = dangerPoints.Length - 1;
+
+        bool noMoreDanger = nbDanger <= 0;
+        if (noMoreDanger)
+        {
+            getNextRoomPoints();
+            return;
+        }
+
+        int randomDangerIndex = Random.Range(1, nbDanger);
+        movePoint = dangerPoints[randomDangerIndex];
+        dangerPoints[randomDangerIndex].gameObject.SetActive(false);
+    }
+
+    private void getNextRoomPoints()
+    {
+        int nbRooms = paths.transform.childCount;
+        bool noMoreRoom = roomIndex + 1 >= nbRooms;
+        if (noMoreRoom)
+        {
+            Debug.Log("End of the path");
+            return;
+        }
+
+        roomIndex++;
+        Debug.Log("Room " + roomIndex);
+
+        Transform roomPathPoints = paths.transform.GetChild(roomIndex);
+        pathPoints = roomPathPoints.GetComponentsInChildren<Transform>();
+        pointIndex = 1; // start at index 1, cause index 0 = the parent itself
+        movePoint = pathPoints[pointIndex];
+    }
+}
