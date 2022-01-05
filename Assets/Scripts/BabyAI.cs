c2eed05f21aefeebbaeb03551a3ce5a92c011b7b
SarahHeo <sarah.heouaine@gmail.com>
2022-01-05 14:09:09 +0100

    merge

--- Assets/Scripts/BabyAI.cs
+++ Assets/Scripts/BabyAI.cs
@@ -8,7 +8,7 @@ using UnityEngine.UI;
 public class BabyAI : MonoBehaviour
 {
     // Suis les points de "paths", puis va chercher un danger random dans la
-    [SerializeField] private GameObject fixedPoints;
+    [SerializeField] private GameObject fixedPoints; // room points
     [SerializeField] private GameObject danger;
     [SerializeField] private NavMeshAgent agent;
     private Transform[] pathPoints;
@@ -19,40 +19,75 @@ public class BabyAI : MonoBehaviour
     private float rotationSpeed = 200f;
     private int roomIndex = -1; // le mettre dans GameManager maybe
     int movePointType;
+    private Transform tempMovePoint = null;
+
+    private Collider currentDanger = null;
 
     void Start()
     {
-        getNextRoomPoints();
+        GetNextRoomPoints();
+
     }
 
     void Update()
     {
-        checkIfHasReachedPoint();
+        MoveToPoint();
+        GoTowardsDangerIfInRange();
+
+        // checkIfHasReachedPoint();
     }
 
-    private void checkIfHasReachedPoint()
+    private void MoveToPoint()
+    {
+        agent.SetDestination(movePoint.position);
+    }
+
+    private void GoTowardsDangerIfInRange()
+    {
+        List<Collider> dangers = GameManager.DangersInRange;
+        Debug.Log(dangers.Count);
+        if (dangers.Count > 0)
+        {
+            if (currentDanger == null)
+            {
+                currentDanger = dangers[0];
+                tempMovePoint = movePoint;
+                movePoint = currentDanger.transform;
+            }
+            else
+            {
+                // rien, on continue
+            }
+
+        }
+        else
+        {
+            currentDanger = null;
+            movePoint = tempMovePoint; // go back to the defined path
+        }
+    }
+
+    /*private void checkIfHasReachedPoint()
     {
         bool hasReachPoint = agent.remainingDistance <= agent.stoppingDistance;
         if (hasReachPoint)
         {
             bool isMovePointADanger = movePointType == 1;
+<<<<<<< Updated upstream
+            bool isDangerInRange = GameManager.GetInstance().IsDangerInRange;
+=======
             bool isDangerInRange = GameManager.IsDangerInRange;
+            Debug.Log(GameManager.IsDangerInRange);
+>>>>>>> Stashed changes
             if (isMovePointADanger && isDangerInRange)
             {
                 return;
             }
             setNextPoint();
         }
-        move();
-    }
+    }*/
 
-
-    private void move()
-    {
-        agent.SetDestination(movePoint.position);
-    }
-
-    private void setNextPoint()
+    /*private void setNextPoint()
     {
         pointIndex++;
         bool noMorePathPoints = pointIndex >= pathPoints.Length;
@@ -62,9 +97,9 @@ public class BabyAI : MonoBehaviour
             return;
         }
         movePoint = pathPoints[pointIndex];
-    }
+    }*/
 
-    private void lookForDanger()
+    /*private void lookForDanger()
     {
         Transform roomDangerPoints = danger.transform.GetChild(roomIndex);
         dangerPoints = roomDangerPoints.GetComponentsInChildren<Transform>();
@@ -81,10 +116,16 @@ public class BabyAI : MonoBehaviour
         movePoint = dangerPoints[randomDangerIndex];
         movePointType = 1;
         agent.stoppingDistance = 2;
-        GameManager.CurrentDanger = movePoint;
-    }
+<<<<<<< Updated upstream
 
-    private void getNextRoomPoints()
+        GameManager.GetInstance().CurrentDanger = movePoint;
+    }
+=======
+        GameManager.CurrentDanger = movePoint;
+    }*/
+
+
+    private void GetNextRoomPoints()
     {
         int nbRooms = fixedPoints.transform.childCount;
         bool noMoreRoom = roomIndex + 1 >= nbRooms;
@@ -101,6 +142,7 @@ public class BabyAI : MonoBehaviour
         pathPoints = roomPathPoints.GetComponentsInChildren<Transform>();
         pointIndex = 1; // start at index 1, cause index 0 = the parent itself
         movePoint = pathPoints[pointIndex];
+        Debug.Log(movePoint);
     }
-    
+
 }
